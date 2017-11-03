6b88dbea6336457de391d1f54fe5f36aad95ffb6
serpent7776 <serpent7776@gmail.com>
2017-11-03 20:36:08 +0100

    translate view to make x=0 at center of screen

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
-local VW, VH, VH2
+local VW, VW2, VH, VH2
 local roadWidthNear, roadWidthFar
 
 function love.load()
 	VW = love.graphics.getWidth()
 	VH = love.graphics.getHeight()
+	VW2 = 0.5 * VW
 	VH2 = 0.5 * VH
 	roadWidthNear = VW * 0.96
 	roadWidthFar = roadWidthNear * 0.2
@@ -11,20 +12,20 @@ end
 
 function love.draw()
 	love.graphics.scale(1, -1)
-	love.graphics.translate(0, -VH)
+	love.graphics.translate(VW2, -VH)
 	love.graphics.setColor(102, 153, 255)
-	love.graphics.rectangle('fill', 0, VH2, VW, VH2)
+	love.graphics.rectangle('fill', -VW2, VH2, VW, VH2)
 	love.graphics.setColor(0, 153, 51)
-	love.graphics.rectangle('fill', 0, 0, VW, VH2)
+	love.graphics.rectangle('fill', -VW2, 0, VW, VH2)
 	drawSegment(0)
 end
 
 function drawSegment(z)
 	love.graphics.setColor(96, 96, 96)
 	love.graphics.polygon('fill',
-		0.5 * (VW - roadWidthNear), 0,
-		0.5 * (VW - roadWidthFar), VH2,
-		0.5 * (VW - roadWidthFar) + roadWidthFar, VH2,
-		0.5 * (VW - roadWidthNear) + roadWidthNear, 0
+		-0.5 * roadWidthNear, 0,
+		-0.5 * roadWidthFar, VH2,
+		 0.5 * roadWidthFar, VH2,
+		 0.5 * roadWidthNear, 0
 	)
 end
