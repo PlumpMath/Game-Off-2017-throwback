7016e3d2c1c053fb093fba7109a8f713b9d76101
Serpent7776 <serpent7776@gmail.com>
2017-11-08 22:12:40 +0100

    improve road building

--- level1.lua
+++ level1.lua
@@ -5,18 +5,34 @@ function straight(r, n)
 	end
 end
 
-function curve(r, n, dx)
+function uncurve(r, n)
+	local dx = r[table.getn(r) - 1]
+	local ddx = (0 - dx) / n
 	for i = 1, n do
+		dx = dx + ddx
+		table.insert(r, dx)
+		table.insert(r, 0)
+	end
+end
+
+function curve(r, n, mdx)
+	local dx = r[table.getn(r) - 1]
+	local ddx = (mdx - dx) / n
+	for i = 1, n do
+		dx = dx + ddx
 		table.insert(r, dx)
 		table.insert(r, 0)
 	end
 end
 
 local r = {
+	0, 0,
 }
 
-straight(r, 10)
-curve(r, 10, 10)
-straight(r, 10)
+straight(r, 9)
+curve(r, 10, 2)
+curve(r, 29, 8)
+uncurve(r, 15)
+straight(r, 16)
 
 return r
